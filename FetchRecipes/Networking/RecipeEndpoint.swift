aae21fcef8b62cba45f1c0ae92d1715cbfca91c5
Momo Khan <mkhanongm@gmail.com>
2025-06-03 15:29:45 -0400

    Add enum for recipe endpoints

---
+++ FetchRecipes/Networking/RecipeEndpoint.swift
@@ -0,0 +1,43 @@
+//
+//  RecipeEndpoint.swift
+//  FetchRecipes
+//
+//  Created by Momo Khan on 6/3/25.
+//
+
+enum RecipeEndpoints: APIEndpoint {
+    case recipes
+    case malformed
+    case empty
+    
+    var path: String {
+        switch self {
+        case .recipes:
+            return "/recipes.json"
+        case .malformed:
+            return "/recipes-malformed.json"
+        case .empty:
+            return "/recipes-empty.json"
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .recipes,
+                .malformed,
+                .empty:
+            return .get
+        }
+    }
+    
+    var mockFile: String {
+        switch self {
+        case .recipes:
+            return "recipes"
+        case .malformed:
+            return "recipes-malformed"
+        case .empty:
+            return "recipes-empty"
+        }
+    }
+}
