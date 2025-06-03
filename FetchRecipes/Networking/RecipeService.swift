1356d278056a6ec6294344e0a938982be451d101
Momo Khan <mkhanongm@gmail.com>
2025-06-03 17:28:33 -0400

    Make a recipe service protocol

--- FetchRecipes/Networking/RecipeService.swift
+++ FetchRecipes/Networking/RecipeService.swift
@@ -7,7 +7,13 @@
 
 import Foundation
 
-final class RecipeService {
+protocol RecipeServiceProtocol {
+    func fetchRecipes() async throws -> [Recipe]?
+    func fetchEmpty() async throws -> [Recipe]?
+    func fetchMalformed() async throws -> [Recipe]?
+}
+
+final class RecipeService: RecipeServiceProtocol {
     private var networkClient: NetworkClientProtocol
     
     init(networkClient: NetworkClientProtocol) {
@@ -15,17 +21,17 @@ final class RecipeService {
     }
     
     func fetchRecipes() async throws -> [Recipe]? {
-        let response = try await networkClient.sendRequest(endpoint: RecipeEndpoints.recipes, response: RecipeResponse.self)
+        let response = try await networkClient.sendRequest(endpoint: RecipeEndpoint.recipes, response: RecipeResponse.self)
         return response.recipes
     }
     
     func fetchEmpty() async throws -> [Recipe]? {
-        let response = try await networkClient.sendRequest(endpoint: RecipeEndpoints.empty, response: RecipeResponse.self)
+        let response = try await networkClient.sendRequest(endpoint: RecipeEndpoint.empty, response: RecipeResponse.self)
         return response.recipes
     }
     
     func fetchMalformed() async throws -> [Recipe]? {
-        let response = try await networkClient.sendRequest(endpoint: RecipeEndpoints.malformed, response: RecipeResponse.self)
+        let response = try await networkClient.sendRequest(endpoint: RecipeEndpoint.malformed, response: RecipeResponse.self)
         return response.recipes
     }
 }
