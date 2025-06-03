c037c697a0eb0a91c1c779fd58d5871abc4133c8
Momo Khan <mkhanongm@gmail.com>
2025-06-03 15:55:07 -0400

    Add mockable protcol and mock network client that uses bundle files to send requests

---
+++ FetchRecipesTests/Mocks/MockNetworkClient.swift
@@ -0,0 +1,18 @@
+//
+//  MockNetworkClient.swift
+//  FetchRecipes
+//
+//  Created by Momo Khan on 6/3/25.
+//
+
+import XCTest
+
+@testable import FetchRecipes
+
+final class MockNetworkClient: Mockable, NetworkClientProtocol {
+    func sendRequest<T: Decodable>(endpoint: APIEndpoint, response: T.Type) async throws -> T {
+        return loadJSON(filename: endpoint.mockFile, type: response.self)
+    }
+    
+    
+}
